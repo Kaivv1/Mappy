f0cf6c3ba64bc9cc8939949cc61b0073ae3cfe20
Kaivv1 <kvalchevv1@gmail.com>
2023-08-11 12:45:41 +0300

    first commit

---
+++ js/config.js
@@ -0,0 +1,9 @@
+export const form = document.querySelector('.form');
+export const containerWorkouts = document.querySelector('.workouts');
+export const inputType = document.querySelector('.form__input--type');
+export const inputDistance = document.querySelector('.form__input--distance');
+export const inputDuration = document.querySelector('.form__input--duration');
+export const inputCadence = document.querySelector('.form__input--cadence');
+export const inputElevation = document.querySelector('.form__input--elevation');
+export const delAllWorkouts = document.querySelector('.btn-delAll');
+// "build": "parcel build index.html --dist-dir ./dist"
---
+++ js/workoutView.js
@@ -0,0 +1,55 @@
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  _workoutId;
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+
+  _setDescription() {
+    // prettier-ignore
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+      months[this.date.getMonth()]
+    } ${this.date.getDate()}`;
+  }
+}
+
+export class Running extends Workout {
+  type = 'running';
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+    this._setDescription();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+export class Cycling extends Workout {
+  type = 'cycling';
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+    this._setDescription();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// export default new Cycling();
+// export default new Running();
+// export default new Workout();
