53d8864065e7a7a439902de2439cacc7b88112a0
Kaloyan Vulchev <kvalchevv1@gmail.com>
2024-02-27 19:40:17 +0200

    Update workoutFunctions.js

--- js/functions/workoutFunctions.js
+++ js/functions/workoutFunctions.js
@@ -10,18 +10,14 @@ import { Cycling } from '../views/cyclingView.js';
 export function newWorkout(e) {
   e.preventDefault();
 
-  // Get data from form
   const type = config.inputType.value;
   const distance = +config.inputDistance.value;
   const duration = +config.inputDuration.value;
   const { lat, lng } = state.mapping.mapEvent.latlng;
   let workout;
 
-  // If workout running, create running object
   if (type === 'running') {
     const cadence = +config.inputCadence.value;
-
-    // Check if data is valid
     if (
       !validInputs(distance, duration, cadence) ||
       !allPositive(distance, duration, cadence)
@@ -31,10 +27,8 @@ export function newWorkout(e) {
     workout = new Running([lat, lng], distance, duration, cadence);
   }
 
-  // If workout cycling, create cycling object
   if (type === 'cycling') {
     const elevation = +config.inputElevation.value;
-
     if (
       !validInputs(distance, duration, elevation) ||
       !allPositive(distance, duration)
@@ -44,18 +38,10 @@ export function newWorkout(e) {
     workout = new Cycling([lat, lng], distance, duration, elevation);
   }
 
-  // Add new object to workout array
   state.storage.workouts.push(workout);
 
-  // Render workout on map as marker
   renderWorkoutMarker(workout);
-
-  // Render workout on list
   renderWorkout(workout);
-
-  // Hide form + clear input fields
   hideForm();
-
-  // Set local storage to all workouts
   setLocalStorage();
 }
